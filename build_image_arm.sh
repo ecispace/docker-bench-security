814c78adcbe5c05f5438a15b04413f7c53d22bc9
soaop <fengjin@uranus.io>
2021-04-21 18:21:32 +0800

    diss: build image arm script and lang pack

--- build_image_arm.sh
+++ build_image_arm.sh
@@ -1,4 +1,4 @@
 
 zip -x "*\.DS_Store" -x "__MACOSX" -r -q trans.zip trans 
 
-docker build -t registry.cn-qingdao.aliyuncs.com/diss/docker-bench-security .
+docker buildx build -t registry.cn-qingdao.aliyuncs.com/diss/docker-bench-security:v.1.3.5 . --platform=linux/arm64 -o type=docker -f Dockerfile.arm
